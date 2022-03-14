e921bf583ce13f518f3b9e9ce533cd55246d7fce
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-03-14 15:45:10 +0100

    Add Noto Sans light, regular, and bold + italics

---
+++ src/fonts.rs
@@ -0,0 +1,13 @@
+//! TTF fonts as byte slies.
+
+pub const NOTO_SANS_REGULAR: &[u8] = include_bytes!("../assets/noto-sans/NotoSans-Regular.ttf");
+pub const NOTO_SANS_REGULAR_ITALIC: &[u8] =
+    include_bytes!("../assets/noto-sans/NotoSans-Italic.ttf");
+
+pub const NOTO_SANS_LIGHT: &[u8] = include_bytes!("../assets/noto-sans/NotoSans-Light.ttf");
+pub const NOTO_SANS_LIGHT_ITALIC: &[u8] =
+    include_bytes!("../assets/noto-sans/NotoSans-LightItalic.ttf");
+
+pub const NOTO_SANS_BOLD: &[u8] = include_bytes!("../assets/noto-sans/NotoSans-Bold.ttf");
+pub const NOTO_SANS_BOLD_ITALIC: &[u8] =
+    include_bytes!("../assets/noto-sans/NotoSans-BoldItalic.ttf");
